acfa6af003e09570574e02999e981c17b107e1dd
dutrinha <kickjp123@gmail.com>
2024-05-15 09:36:15 -0300

    Initial commit

---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar"
+
+function Header() {
+    return (
+        <header className="flex">
+            <h1> Bem-vindo ao DutrinhaFLIX</h1><br/>
+            <Navbar/>
+        </header>
+    )
+}
+
+export default Header
